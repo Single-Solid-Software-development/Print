4ac9380607e09b82f520ea2cf6b0c7e9af627e93
mr-shoxruxbek <shohruh.maxmudov@mail.ru>
2020-10-14 12:32:48 +0500

    removed resouse

--- app/src/main/java/uz/ssd/canvas/MainActivity.kt
+++ app/src/main/java/uz/ssd/canvas/MainActivity.kt
@@ -3,32 +3,20 @@ package uz.ssd.canvas
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Paint
-import android.graphics.pdf.PdfDocument
 import android.os.Bundle
-import android.os.Environment
 import android.print.PrintAttributes
 import android.print.PrintJob
 import android.print.PrintManager
-import android.util.Log
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import uz.ssd.canvas.Html.DESKTOP_USER_AGENT
 import uz.ssd.canvas.Html.htmlCss
 import uz.ssd.canvas.Html.htmlString
-import uz.ssd.canvas.util.DownloadIntegrator
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 
-
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +41,6 @@ class MainActivity : AppCompatActivity() {
         builder.setMediaSize(PrintAttributes.MediaSize.ISO_A5)
         builder.setColorMode(PrintAttributes.COLOR_MODE_MONOCHROME)
         builder.setMinMargins(PrintAttributes.Margins.NO_MARGINS)
-//        builder.setResolution(Resolution("zoom", PRINT_SERVICE, 600, 600))
 
         val printJob: PrintJob = printManager.print(jobName, printAdapter, builder.build())
         if (printJob.isCompleted) showMessage("print_complete")
@@ -62,11 +49,6 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun download() {
-        GlobalScope.launch {
-            val json = DownloadIntegrator(applicationContext).readCategory()
-            Log.d("$$$$", json)
-        }
-
         webView.loadDataWithBaseURL(
             htmlCss, htmlString, "text/html", "UTF-8", null
         )
@@ -77,8 +59,8 @@ class MainActivity : AppCompatActivity() {
                 this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED
-        ) create()
-        else ActivityCompat.requestPermissions(
+        ) {
+        } else ActivityCompat.requestPermissions(
             this,
             arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
             PackageManager.PERMISSION_GRANTED
@@ -103,34 +85,6 @@ class MainActivity : AppCompatActivity() {
             webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
 
             webView.loadUrl("https://github.com/mr-shoxruxbek/TestCss/blob/main/pdf2html_1310/index.html")*/
-    }
-
-    private fun create() {
-        val pdf = PdfDocument()
-        val paint = Paint()
-
-        val pageInfo = PdfDocument.PageInfo.Builder(418, 598, 1).create()
-        val page1 = pdf.startPage(pageInfo)
-
-        val canvas = page1.canvas
-        paint.textSize = 7f
-        canvas.drawText("Hello", 36.72f, 62f, paint)
-        pdf.finishPage(page1)
-
-        val file = File(Environment.getExternalStorageDirectory().absolutePath, "/Hello.pdf")
-
-        /*folder.mkdirs()
-
-        val file = File(folder, "Hello.pdf")
-        if (file.exists ()) file.delete ()
-        file.createNewFile()*/
-        try {
-            pdf.writeTo(FileOutputStream(file))
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        showMessage("DONE")
-        pdf.close()
     }
 
     private fun showMessage(message: String) =
