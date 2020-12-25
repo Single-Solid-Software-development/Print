d185c1a141c48d7c4b1c989cba27ac99cd1ef813
mr-shoxruxbek <shohruh.maxmudov@mail.ru>
2020-12-25 10:58:17 +0500

    error logs showed

--- app/src/main/java/uz/ssd/canvas/base/BaseActivity.kt
+++ app/src/main/java/uz/ssd/canvas/base/BaseActivity.kt
@@ -62,13 +62,42 @@ abstract class BaseActivity : AppCompatActivity() {
 //        startActivity(Intent(this, activity))
 //    }
     protected fun checkMessage() {
-        val error1 = "2fYontnae0uGRMj+2lTSQQr2MD3ec+uAEeb0RcIjk0Pmp00tiJfCg5JLmbLxIwvvFQ5siHK9yjAgUVHoXbkJ68h0FrVD2AIQ"
-        // Не удалось получить информацию о карте
-        val error2 = "2fYontnae0uGRMj+2lTSQWchwe510eOM5/DF2UaW4hFaPFd0/bPNqDKzVHTU6sw8d3CDihWguxS71kx/UVf0Ew=="
-        // Не удалось зарегистрировать карту
-        val error3 = "CCBGeA8+wJ4HwkJDKd7BRjVsCiSaBgGJR0x8d1iJMVVnczPluEtaQA=="
-        //Недостаточно средств
+    val error1 = "2fYontnae0uGRMj+2lTSQQr2MD3ec+uAEeb0RcIjk0Pmp00tiJfCg5JLmbLxIwvvFQ5siHK9yjAgUVHoXbkJ68h0FrVD2AIQ"
+// Не удалось получить информацию о карте
+    val error2 = "2fYontnae0uGRMj+2lTSQWchwe510eOM5/DF2UaW4hFaPFd0/bPNqDKzVHTU6sw8d3CDihWguxS71kx/UVf0Ew=="
+// Не удалось зарегистрировать карту
+    val error3 = "CCBGeA8+wJ4HwkJDKd7BRjVsCiSaBgGJR0x8d1iJMVVnczPluEtaQA=="
+//Недостаточно средств
+    val error4 = "0jglufORKkRltB+rMEZA/Oceyu76o+aXukfIxblK/sCuQRJHcRuXDILrRjwnRQUK"
+// Failed to connect to agr.uz/83.221.180.212:443
 
-        Log.d("$$$$$", TripleDES.decrypt(error3))
+    val error5 = "2fYontnae0uGRMj+2lTSQWchwe510eOM5/DF2UaW4hFaPFd0/bPNqDKzVHTU6sw8d3CDihWguxS71kx/UVf0Ew=="
+// Не удалось зарегистрировать карту
+    val error6 = "BB5s9Nl0243tMxIax/eWTHwMfdZFnY8LqI7UvZH9DbYQ6vK09DMmxw=="
+//    The callable returned a null value
+    val error7 = "BB5s9Nl0243tMxIax/eWTHwMfdZFnY8LqI7UvZH9DbYQ6vK09DMmxw=="
+//    Недостаточно средств
+    val error8 = "CCBGeA8+wJ4HwkJDKd7BRjVsCiSaBgGJR0x8d1iJMVVnczPluEtaQA=="
+//    Connection closed by peer
+    val error9 = "dMsuGs2pHp/I17P3lEjPfWUWfsaJCtLP0AwJZZT5Dsg="
+//    Неверный формат номер карты
+    val error10 = "GAaPulefz1NNodTg7iZOJET1Gq4R5MEm8kCHBPAEzfgyiooxxkN2ZQ17lz/6vGWDlirYsQTEg18="
+//    Не найден номер клиента
+    val error11 = "iOTpzKtQCBgOwRkSJI3MuO3rDyk/5LxIJPZ/0QtOwrRznEC3LoKVbHnetUaJ+4Vl"
+//    Unable to resolve host "agr.uz": No address associated with hostname
+    val error12 = "MTwqcl3H8akvBsf5HPD0UnROBQSTpCVSfCNG8Irz8s9/uh4wV7wFOXMwtoWwC0ZgLXYL4s5cCyOOAgeCnuXLvhBuGE4OaWQy"
+//    Ошибка! Не найден абонент
+    val error13 = "YHoHM3Mo1EMN6bC6hwjebTZlE4i63S64rkJLvSnXMa8Cs+KM6y88G60hoN7+UnJp"
+
+        Log.d("$$$$$", TripleDES.decrypt(error4))
+        Log.d("$$$$$", TripleDES.decrypt(error5))
+        Log.d("$$$$$", TripleDES.decrypt(error6))
+        Log.d("$$$$$", TripleDES.decrypt(error7))
+        Log.d("$$$$$", TripleDES.decrypt(error8))
+        Log.d("$$$$$", TripleDES.decrypt(error9))
+        Log.d("$$$$$", TripleDES.decrypt(error10))
+        Log.d("$$$$$", TripleDES.decrypt(error11))
+        Log.d("$$$$$", TripleDES.decrypt(error12))
+        Log.d("$$$$$", TripleDES.decrypt(error13))
     }
 }
